c66e6d1de49b49546d0fa01f4ea7d07479f14393
Oleg <boxmilo@gmail.com>
2018-06-14 19:11:27 +0600

    Правки

--- PluginAd.class.php
+++ PluginAd.class.php
@@ -51,14 +51,15 @@ class PluginAd extends Plugin
 
     public function Activate()
     {
-        $oType = Engine::GetEntity('ModuleTopic_EntityTopicType');
-        $oType->_setDataSafe(['name' =>"Объявление", 'name_many' =>'Объявления', 'code' => 'ad' ]);
-        $oType->setParams(['allow_text' => 1, "allow_tags" => 1, "allow_deferred_all" => 1, "css_icon" =>'']);
-        $oType->setAllowRemove(1);
-        $oType->setState( ModuleTopic::TOPIC_TYPE_STATE_ACTIVE );
-        $oType->setDateCreate(date("Y-m-d H:i:s"));
-        $this->Topic_AddTopicType($oType);
-        //$this->Category_CreateTargetType('specialization', 'Специализации', array(), true);
+//        $oType = Engine::GetEntity('ModuleTopic_EntityTopicType');
+//        $oType->_setDataSafe(['name' =>"Объявление", 'name_many' =>'Объявления', 'code' => 'ad' ]);
+//        $oType->setParams(['allow_text' => 1, "allow_tags" => 1, "allow_deferred_all" => 1, "css_icon" =>'']);
+//        $oType->setAllowRemove(1);
+//        $oType->setState( ModuleTopic::TOPIC_TYPE_STATE_ACTIVE );
+//        $oType->setDateCreate(date("Y-m-d H:i:s"));
+//        $this->Topic_AddTopicType($oType);
+        
+        $this->Category_CreateTargetType('specialization', 'Специализации', array(), true);
         
         $aProperties = array(
             array(
